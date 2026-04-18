439793ea276622f502b452594dd1ece5857919a6
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-04-18 18:49:59 +0500

    feat: implement render cards

--- src/components/ui/GridCard.jsx
+++ src/components/ui/GridCard.jsx
@@ -1,10 +1,16 @@
 import Dropdown from '@components/ui/Dropdown';
+import ProductCard from '@components/common/ProductCard';
+import { getStats } from '@lib/utils';
 
 const GridCard = () => {
+  const tvProducts = getStats().filtered.tv;
   return (
     <div className="w-full h-full flex flex-col gap-6">
       <header className="flex gap-2 items-center">
-        <p className="text-black/50"><span>8</span> products</p>
+        <p className="text-black/50">
+          <span>{tvProducts.length} </span>
+          {tvProducts.length === 1 ? 'product' : 'products'}
+        </p>
         <p className="ml-auto">Sort by:</p>
         <Dropdown
           placeholder="Low to High"
@@ -12,7 +18,9 @@ const GridCard = () => {
         />
       </header>
       <div className="grid grid-cols-4 gap-4">
-
+        {tvProducts.map((product) => (
+          <ProductCard key={product.id} {...product} />
+        ))}
       </div>
     </div>
   );
