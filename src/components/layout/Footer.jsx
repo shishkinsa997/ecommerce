74ebf8b894dd2239d2b17fca3d660220aeb88519
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-04-20 02:17:59 +0500

    fix: update adaptive layout for product grid, header, footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="relative w-full mx-auto mt-auto after:absolute after:top-0 after:left-0 after:w-full after:h-0.5 after:bg-black/10 after:content-['']">
       <div className="max-w-360 mx-auto p-8 flex flex-col items-center">
-        <div className="w-full grid grid-cols-4 gap-8 mb-4">
+        <div className="w-full grid grid-cols-4 gap-8 mb-4 max-lg:grid-cols-2 max-md:grid-cols-1">
           <div className="flex flex-col gap-2">
             <h3 className="text-lg">About</h3>
             <ul className="flex flex-col gap-2 text-black/50">
@@ -35,8 +35,10 @@ const Footer = () => {
             <h3 className="text-lg">Newsletter</h3>
             <form action="#" className="flex flex-col gap-2 text-black/50">
               <label htmlFor="subscribe">Subscribe for exclusive deals</label>
-              <input type='email' id="subscribe" placeholder="Enter your email" className='rounded-xl ring-1 ring-black/10 bg-black/5 focus:outline-none px-3 py-2'/>
-              <Button variant='primary'>Subscribe</Button>
+              <div className="flex flex-col gap-2 max-lg:flex-row">
+                <input type='email' id="subscribe" placeholder="Enter your email" className=' w-full rounded-xl ring-1 ring-black/10 bg-black/5 focus:outline-none px-3 py-2'/>
+                <Button variant='primary'>Subscribe</Button>
+              </div>
             </form>
           </div>
         </div>
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,23 +1,64 @@
+import { useState, useEffect } from 'react';
 import { ShoppingCart, User } from 'lucide-react';
+import { cn } from '@lib/utils';
+
 const Header = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 640);
+  const activeStyle = 'text-black max-sm:border-b-3 max-sm:border-black'
+  const tabStyles = 'block text-center items-center my-auto w-full max-sm:text-[16px]/10 max-sm:h-10'
+  const underlineStyle = 'after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-black/10 after:content-[""]'
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 640);
+    };
+
+    window.addEventListener('resize', handleResize);
+  }, []);
+  const nav = (
+    <nav className={cn(
+      'relative bg-white w-full mx-auto max-sm:px-4'
+      , isMobile ? underlineStyle : null
+    )}>
+      <ul className="flex items-center justify-between w-full gap-6 text-gray-500 max-sm:gap-4 max-sm:py-3">
+        <li className='flex-1'><a href="/tv"
+        className={cn(
+          activeStyle,
+          tabStyles
+        )}
+        >TV</a></li>
+        <li className='flex-1'><a href="/phone"
+        className={cn(
+          tabStyles
+        )}
+        >Phone</a></li>
+        <li className='flex-1'><a href="/laptop"
+        className={cn(
+          tabStyles
+        )}
+        >Laptop</a></li>
+      </ul>
+    </nav>
+  );
   return (
-    <header className="sticky z-1 top-0 mx-auto w-full bg-white
-    after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-black/10 after:content-['']">
+    <>
+    <header className={cn(
+          "sticky z-1 top-0 mx-auto w-full max-sm:pb-3 bg-white",
+          underlineStyle
+        )}>
       <div className="max-w-360 mx-auto px-8 py-4 flex items-center justify-between">
-          <nav className="flex items-center gap-8">
+          <div className="flex items-center gap-8">
             <a href="/tv" className="text-[24px]/8">TechStore</a>
-            <ul className="flex items-center gap-6 text-gray-500">
-              <li><a href="/tv" className="text-black">TV</a></li>
-              <li><a href="/phone">Phone</a></li>
-              <li><a href="/laptop">Laptop</a></li>
-            </ul>
-          </nav>
+            {!isMobile && nav}
+          </div>
           <div className="flex items-center gap-4">
             <button className="flex items-center justify-center size-9"><ShoppingCart /></button>
             <button className="flex items-center justify-center size-9"><User /></button>
           </div>
       </div>
     </header>
+    {isMobile && nav}
+    </>
   );
 }
 
